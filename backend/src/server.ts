01e343233226cca77347fb35e42e68661750ca9d
Andrew Matheus <andrewmatheus@gmail.com>
2020-06-15 21:35:30 -0300

    Estrutura inicial

---
+++ backend/src/server.ts
@@ -0,0 +1,33 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import AppError from './errors/AppError';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('BackEnd Started!');
+});
